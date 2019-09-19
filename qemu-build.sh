98c1fdb0f32345798e939209365449368b38c112
andrew <iiandrewchan@gmail.com>
2019-09-19 16:50:50 +0000

    place build qemu into a function and add a reset edu option

--- qemu-build.sh
+++ qemu-build.sh
@@ -2,9 +2,18 @@
 
 HOME="/home/andrew/fileshare"
 
-if [[ $1 == "build" ]]; then
-
+function build_qemu {
     cd $HOME/qemu-pci-emulator/qemu/build
     ../configure --target-list=x86_64-softmmu
-    make -j5
+    make -j
+}
+
+if [[ $1 == "build" ]]; then
+    cp qemu_devices/edu.c qemu/hw/misc/
+    build_qemu
+fi
+
+if [[ $1 == "reset" ]]; then
+    cp qemu_devices/edu_original.c qemu/hw/misc/edu.c
+    build_qemu
 fi
