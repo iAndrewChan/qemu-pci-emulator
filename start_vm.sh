356882fbb912cc89a19cace791346dba8f2ee5eb
andrew <iiandrewchan@gmail.com>
2019-09-20 15:47:13 +0000

    add install mode for tiny core linux, and set up persistent OS

--- start_vm.sh
+++ start_vm.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+
+if [[ $1 == "install" ]]; then
+    qemu/build/x86_64-softmmu/qemu-system-x86_64 \
+    -cdrom core.iso \
+    -m 512 \
+    -hda disk.qcow2 \
+    -device e1000 \
+    -device edu
+    exit
+fi
+
+# Load OS onto RAM
 qemu/build/x86_64-softmmu/qemu-system-x86_64 \
--cdrom core.iso \
 -m 512 \
+-hda disk.qcow2 \
 -device e1000 \
 -device edu
