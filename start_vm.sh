14f88b505ebef0b47d57a80f8743476a4ec7bbae
andrew <iiandrewchan@gmail.com>
2019-09-23 10:07:22 +0000

    * added create disk image when start_vm.sh install is run * changed comment from load vm to be start vm

--- start_vm.sh
+++ start_vm.sh
@@ -2,6 +2,10 @@
 
 
 if [[ $1 == "install" ]]; then
+    
+    # Create disk image
+    qemu/build/qemu-img create -f qcow2 disk.qcow2 500M
+
     qemu/build/x86_64-softmmu/qemu-system-x86_64 \
     -cdrom core.iso \
     -m 512 \
@@ -11,7 +15,7 @@ if [[ $1 == "install" ]]; then
     exit
 fi
 
-# Load OS onto RAM
+# Start up vm
 qemu/build/x86_64-softmmu/qemu-system-x86_64 \
 -m 512 \
 -hda disk.qcow2 \
